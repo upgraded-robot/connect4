7044d3040a9afbc5582e4784b68adfe83cba19fb
avanegasp <arco527@gmail.com>
2016-09-26 13:39:09 -0500

    estructura inicial js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function Game (){
+
+}
+
+
+Game.prototype.turn = function(player){
+	
+}
+
+Game.prototype.dropChip = function(){
+
+}
+
+Game.prototype.gameState = function(){
+
+}
+
+function Player (color){
+	this.color= color;
+}
+
+function Board () {
+	column()
+}
+
+function Column(){
+
+}
+
+
